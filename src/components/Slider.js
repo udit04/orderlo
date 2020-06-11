c59d9142b3ac68e0d44029167ed300dd25d448bd
amanrathore10 <amansat.rathore@gmail.com>
2020-06-12 00:58:09 +0530

    added images slide and login

---
+++ src/components/Slider.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Slider from "react-slick";
+import styled from 'styled-components'
+// import sliderimg from '../../public/static/images/'
+import img1 from '../../public/static/image1.png'
+export default class SimpleSlider extends React.Component {
+  render() {
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+        <>
+      <Slider {...settings}>
+        <Slide>
+            <img src={require("../../public/static/image1.png")}/>
+        </Slide>
+        <Slide>
+        <img src={require("../../public/static/image2.png")}/>        
+        </Slide>
+        <Slide>
+        <img src={require("../../public/static/image3.png")}/>
+        </Slide>
+      </Slider>
+      </>
+    );
+  }
+}
+
+const Slide = styled.div`
+display:block;
+width:100%;
+min-height:400px;
+background:#6d83db;;
+img{
+    display:block;
+    width:100%;
+    height:100%;
+}
+`;
