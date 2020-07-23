4eba0fec6adf21f381a0729fe10f3f088a1802e0
Udit Agrawal <udit.agrawal@rebelfoods.co>
2020-07-23 11:46:40 +0530

    index page to content webiste

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,15 @@ import Router from 'next/router'
 export default function Index() {
   const {authData,setauthData} = useContext(AuthContext);
   useEffect(() => {
-    if(authData.user_id){
-      Router.push('/store/1');
-    }
+    // if(authData.user_id){
+    //   Router.push('/store/1');
+    // }
+    window.location.href = 'https://kushal98.github.io/Ordrlo-Website/';
     return () => {
     }
   }, [])
 
+  return <></>;
   return (
       <Block >
         {/* <Login /> */}
